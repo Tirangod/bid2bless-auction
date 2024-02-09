32104c49977d2fcfb476380cd5b1f63f1726cbc9
Tirangod <tirangodua@gmail.com>
2024-02-09 15:00:27 +0200

    MVC pattern files added

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bid2bless/src/back/routes"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
+	app := fiber.New(fiber.Config{})
 	
+	routes.SetupRoutes(app)
+
+	err := app.Listen(":3000")
+	log.Fatal(err)
 }

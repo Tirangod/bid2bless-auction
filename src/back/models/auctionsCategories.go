92df2b3b462c1c014175d172ec1980b74c80745a
Tirangod <tirangodua@gmail.com>
2024-02-09 15:41:54 +0200

    Change fiels to private

--- src/back/models/auctionsCategories.go
+++ src/back/models/auctionsCategories.go
@@ -1,7 +1,7 @@
 package models
 
 type AuctionsCategories struct {
-	ConnectionId int `json:"connection_id"`
-	AuctionId    int `json:"auction_id"`
-	UserId       int `json:"user_id"`
+	connectionId int `json:"connection_id"`
+	auctionId    int `json:"auction_id"`
+	userId       int `json:"user_id"`
 }

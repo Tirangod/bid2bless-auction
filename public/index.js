f7cf63b7cc2f5468ae5010247647a88c1ac264b0
Misha Zubritsky <zubritsky.misha@gmail.com>
2024-02-10 23:12:38 +0200

    tired bear work

---
+++ public/index.js
@@ -0,0 +1,22 @@
+console.log("index.js: started!")
+
+import Alpine from './js/alpinejs/alpinejs.js'
+
+async function loadHTMLComponent(name) {
+    // TODO: handle errors
+    let html = ""
+    await fetch("components/" + name + ".html").
+        then(value => value.text()).catch(err => console.error(err))
+        then(text => html = text).catch(err => console.error(err))
+    return html
+}
+
+Alpine.directive("comp", async (el, { expression }, { evaluate }) => {
+    const compName = expression
+    const html = await loadHTMLComponent(compName)
+    el.innerHTML = html
+})
+
+window.Alpine = Alpine
+ 
+Alpine.start()

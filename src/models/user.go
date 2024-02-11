cb1dbfc7a661a108ecc1a30347b26a965fa79388
Tirangod <tirangodua@gmail.com>
2024-02-11 19:22:55 +0200

    User updated

--- src/models/user.go
+++ src/models/user.go
@@ -1,7 +1,5 @@
 package models
 
-import "github.com/go-playground/validator/v10"
-
 type User struct {
 	ID           int    `validate:"unique"`
 	Email        string `validate:"email"`
@@ -10,8 +8,6 @@ type User struct {
 	Balance      int    `validate:"gte:0"`
 	InuseBalance int    `validate:"gte:0"`
 }
-
-var validate *validator.Validate = validator.New(validator.WithRequiredStructEnabled())
 
 func (u *User) CreateUser() error {
 	// db query

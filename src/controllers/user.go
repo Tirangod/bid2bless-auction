5231317fc162d7ce57920b72fdc08b33a0fa172f
Tirangod <tirangodua@gmail.com>
2024-02-11 18:44:54 +0200

    models/user.go updated

--- src/controllers/user.go
+++ src/controllers/user.go
@@ -1,27 +1,31 @@
 package controllers
 
 import (
+	"bid2bless/src/models"
 	"encoding/json"
-	"fmt"
 )
 
 // User registration request only created when user tries to register
 type userSignupData struct {
-	Username  string `json:"username"`
+	Name      string `json:"username"`
 	Email     string `json:"email"`
 	LoginHash int64  `json:"login_hash"`
 }
 
-// type userLoginData struct {
-// 	LoginHash int64 `json:"login_hash"`
-// }
+type userLoginData struct {
+	LoginHash int64 `json:"login_hash"`
+}
 
 func UserSignup(rawData []byte) error {
-	var u userSignupData
-	err := json.Unmarshal(rawData, &u)
+	var userData userSignupData
+	err := json.Unmarshal(rawData, &userData)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Login: %s, %s, %d;\n", u.Username, u.Email, u.LoginHash)
-	return nil
+	err = (&models.User{
+		Name:         userData.Name,
+		Email:        userData.Email,
+		LoginHash:    userData.LoginHash,
+	}).CreateUser()
+	return err
 }
--- src/models/user.go
+++ src/models/user.go
@@ -1,11 +1,23 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type User struct {
-	Email          string `validate:"email"`
-	Name           string `validate:"required"`
-	HashedPassword int64  `validate:"required"`
-	Balance        int    `validate:"gte:0"`
-	InuseBalance   int    `validate:"gte:0"`
+	ID           int    `validate:"unique"`
+	Email        string `validate:"email"`
+	Name         string `validate:"required"`
+	LoginHash    int64  `validate:"required"`
+	Balance      int    `validate:"gte:0"`
+	InuseBalance int    `validate:"gte:0"`
 }
 
-var _ User
+var validate *validator.Validate = validator.New(validator.WithRequiredStructEnabled())
+
+func (u *User) CreateUser() error {
+	// db query
+	return nil
+}
+
+func (u *User) GetUserByName(name string) {
+
+}
